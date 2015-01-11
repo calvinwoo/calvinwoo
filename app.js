194dd0030433758cbf22b4f6b4ce68c8bbe7f318
Calvin Woo <calvin.woo@citrix.com>
2015-01-11 10:00:30 -0800

    Modified port.

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
 var express = require('express');
 var app = express();
 
+// set the port of our application
+// process.env.PORT lets the port be set by Heroku
+var port = process.env.PORT || 8080;
+
 app.get('/', function (req, res) {
   res.send('Hello World!');
 });
 
-var server = app.listen(3000, function () {
-  var host = server.address().address;
-  var port = server.address().port;
+var server = app.listen(port, function () {
+  console.log('App is running on ' + port);
 });
